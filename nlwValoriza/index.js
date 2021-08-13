13ef643bdfacfa2ca63edcb4beaaa6703d6a22bf
Vinicius Gaspar Siqueira <vgsiqueira94@gmail.com>
2021-08-13 14:23:30 -0300

    subindo arquivos

---
+++ nlwValoriza/index.js
@@ -0,0 +1,4 @@
+function enviarEmail(nome, email) {
+    console.log("Enviar e-mail para " + nome + " com e-mail " + email);
+}
+enviarEmail("Vinicius", "vgsiqueira94@gmail.com");
---
+++ nlwValoriza/index.ts
@@ -0,0 +1,5 @@
+function enviarEmail(nome: string, email: string) {
+  console.log("Enviar e-mail para " + nome + " com e-mail " + email);
+}
+
+enviarEmail("Vinicius", "vgsiqueira94@gmail.com");
